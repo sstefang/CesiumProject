7177d2f256d2e4e964d5da622cdc127c1547544f
Stefan Gheti <stefangheti@Stefans-MacBook-Pro.local>
2020-10-20 21:41:43 +0300

    Ultima Versiune

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AngularCesiumModule, AngularCesiumWidgetsModule} from 'angular-cesium';
 
 import { AppComponent } from './app.component';
+import {CesiumComponent} from './cesium/cesium.component';
+import {RocketComponent} from './rocket/rocket.component';
+import {PolylineComponent} from './polyline/polyline.component';
+import {RouterModule} from '@angular/router';
+import {TrackEntityLayerComponent} from './track-entity-layer/track-entity-layer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CesiumComponent,
+    PolylineComponent,
+    TrackEntityLayerComponent,
+    RocketComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularCesiumModule.forRoot(),
+    AngularCesiumWidgetsModule,
+    RouterModule.forRoot( [
+      { path: 'cesium', component: CesiumComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/polyline/polyline.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { AcNotification, ActionType } from 'angular-cesium';
+import { map } from 'rxjs/operators';
+import { from, Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'polyline-layer-example',
+  template: `
+      <ac-layer acFor="let polyline of polylines$" [context]="this" [show]="show">
+          <ac-polyline-desc props="{
+              width : 8,
+              positions: polyline.positions,
+              material: polyline.material,
+            }">
+          </ac-polyline-desc>
+      </ac-layer>
+  `,
+})
+export class PolylineComponent implements OnInit {
+  polylines$: Observable<AcNotification>;
+
+  entities = [
+    {
+      id: '1',
+      material: Cesium.Color.RED.withAlpha(0.5),
+      positions: Cesium.Cartesian3.fromDegreesArrayHeights([28.720086, 44.407279, 0, 35.55776, 42.92069,  100000]),
+    },
+    {
+      id: '2',
+      material: Cesium.Color.RED.withAlpha(0.5),
+      positions: Cesium.Cartesian3.fromDegreesArrayHeights([35.55776, 42.92069, 100000, 103.4093, 0.3247273,  310000])
+    },
+    {
+      id: '3',
+      material: Cesium.Color.RED.withAlpha(0.5),
+      positions: Cesium.Cartesian3.fromDegreesArrayHeights([ 103.4093, 0.3247273,  310000, 143.6632, -30.16833,  330000,])
+    },
+    {
+      id: '4',
+      material: Cesium.Color.RED.withAlpha(0.5),
+      positions: Cesium.Cartesian3.fromDegreesArrayHeights([ 143.6632, -30.16833,  330000, 261.4367, -14.80083,  350000,])
+    },
+    {
+      id: '5',
+      material: Cesium.Color.RED.withAlpha(0.5),
+      positions: Cesium.Cartesian3.fromDegreesArrayHeights([  261.4367, -14.80083,  350000, -31.23912, 34.43009, 350000,])
+    },
+    {
+      id: '6',
+      material: Cesium.Color.RED.withAlpha(0.5),
+      positions: Cesium.Cartesian3.fromDegreesArrayHeights([  -31.23912, 34.43009, 350000, 29.40542, 43.81517, 370000,])
+    }
+  ];
+  Cesium = Cesium;
+  show = true;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.polylines$ = from(this.entities).pipe(map(entity => ({
+      id: entity.id,
+      actionType: ActionType.ADD_UPDATE,
+      entity: entity,
+    })));
+  }
+
+  setShow($event: boolean) {
+    this.show = $event;
+  }
+
+}
